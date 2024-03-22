c5a9afd68a9d8572187a6c6d22fbb6d42ec380e7
Humayun Kabir <mdhumayunkabir203@gmail.com>
2024-03-22 20:12:46 +0600

    Update 5.find the biggest and smallest numbers from a list of numbers.c

--- 5.find the biggest and smallest numbers from a list of numbers.c
+++ 5.find the biggest and smallest numbers from a list of numbers.c
@@ -1,1 +1,31 @@
 
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for( int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+    
+    int biggest = arr[0], smallest = arr[0];
+
+    for( int i=0; i<n; i++)
+    {
+        if (arr[i]>biggest)
+            biggest = arr[i];
+    }
+
+    for( int i=0; i<n; i++)
+    {
+        if (arr[i]<smallest)
+            smallest = arr[i];
+    }
+    printf("Biggest Number is %d. \nSmallest Number is %d.", biggest, smallest);
+
+    return 0;
+}
