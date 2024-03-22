ae52762d5351b8fd5ded006b99ec6e5a5df1e3c3
Humayun Kabir <mdhumayunkabir203@gmail.com>
2024-03-22 20:02:47 +0600

    Update 5.find the biggest and smallest numbers from a list of numbers.c

--- 5.find the biggest and smallest numbers from a list of numbers.c
+++ 5.find the biggest and smallest numbers from a list of numbers.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
 
-int main()
-{
-    int i,j, y=6;
-    for (i = 1; i<=5; i++)
-    {
-        printf("%d\n", i);
-        if(y<=8) continue;
-        printf("%d\n",y);
-    }
-    return 0;
-}
